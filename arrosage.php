3c25afbea85dc608b3b19df79a2f445ce1ac8cf6
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-05-30 08:52:11 +0200

    Add the kata arrosage

---
+++ arrosage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Créer une fonction arroser() qui le tableau qui suit en paramètre et arrose 
+ * chaque plante de 20cl et fait donc augmenter leur niveau d'eau d'autant.
+ * 
+ * Puis hors de la fonction afficher les plantes par ordre alphabétique
+ * et leur nouveau niveau d'eau
+ */
+$plants = [
+    ['name'=> 'Ficus', "waterLevel" => 54 ],
+    ['name'=> 'Cactus', "waterLevel" => 18 ],
+    ['name'=> 'Monstera', "waterLevel" => 37 ],
+    ['name'=> 'Plante-paon', "waterLevel" => 23 ],
+    ['name'=> 'Aglaonème', "waterLevel" => 68 ],
+    ['name'=> 'Echeveria', "waterLevel" => 41 ],
+    ['name'=> 'Hortensia', "waterLevel" => 7 ],
+];
